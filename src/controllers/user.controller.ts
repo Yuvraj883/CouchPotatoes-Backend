fe1eaea8c85810590961c281160e28b9ea64c5dc
Yuvraj Singh <yuvraj.singh@xarterian.com>
2025-01-17 15:43:29 +0530

    Created userDetails function

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,20 @@
+import Users from "../models/users";
+import { Express, Request, Response } from "express";
+
+const userDetails = async (req: Request, res: Response) => {
+    const user = req.user;
+    try{
+        if(user){
+            const userDetails = await Users.findOne({email:user.email}); 
+            res.status(200).json({user:userDetails});
+
+        }
+        else{
+            res.status(401).json({message:"User not found"});
+        }
+    }
+    catch(err:any){
+        console.error("Error while fetching user details:", err);
+        res.status(500).json({message:"Internal server error", error:err.message})
+    }
+}
