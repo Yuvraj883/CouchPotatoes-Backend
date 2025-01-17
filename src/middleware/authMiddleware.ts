e3123334c361fb2750afc5d08021bdaabe2e17bc
Yuvraj Singh <yuvraj.singh@xarterian.com>
2025-01-17 15:42:52 +0530

    Added verifyToken function

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+
+
+
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+    
+    const token = req.headers.authorization?.split(" ")[1];
+    try{
+    if (!token) {
+        res.status(401).json({ message: "Unauthorized" });
+        return;
+    }
+    if (!process.env.JWT_SECRET) {
+        res.status(500).json({ message: "Internal Server Error" });
+        return;
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)as {email:string, name:string};
+    req.user = decoded;
+
+    next();
+} catch (err: any) {
+
+    console.log("Error while verifying token:", err);
+    res.status(404).json({error:err.message})
+}
+
+
+}
