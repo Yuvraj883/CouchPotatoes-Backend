9c2779ed7ce4def8fd22c6db49bdd24fedc3f5a5
Yuvraj Singh <yuvraj.singh@xarterian.com>
2025-01-17 15:49:11 +0530

    created user routes

---
+++ src/router/user.router.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { verify } from "jsonwebtoken";
+import { verifyToken } from "../middleware/authMiddleware";
+import { userDetails } from "../controllers/user.controller";
+
+const router = express.Router();
+
+router.get('/details', verifyToken, userDetails);
