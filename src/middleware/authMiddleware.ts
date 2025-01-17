375206489a2a02cd3a94c4a4c51bd36271dadc20
Yuvraj Singh <yuvraj.singh@xarterian.com>
2025-01-17 15:46:36 +0530

    Created verifyToken function

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from "express";
 
 
 
-const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     
     const token = req.headers.authorization?.split(" ")[1];
     try{
