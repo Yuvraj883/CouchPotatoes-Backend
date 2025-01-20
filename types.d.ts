4e29fc1dd59e9b19403aa6ebe95f061fa8b1d511
Yuvraj Singh <yuvraj.singh@xarterian.com>
2025-01-20 16:49:08 +0530

    Extends Request class of Express

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,11 @@
-declare namespace Express{
-    export interface Request{
-        user?:{
-            // id:string,
-            email:string,
-            name:string
-        }
+// types.d.ts
+/// <reference types="express" />
+
+declare global {
+    namespace Express {
+      interface Request extends Express.Request {
+        user?: any; // You can specify a more specific type for `user` if needed
+      }
     }
-}
+  }
+  
