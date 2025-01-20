8b479074aec44635e122b19d5c0de8469ee643f2
Yuvraj Singh <yuvraj.singh@xarterian.com>
2025-01-20 16:46:18 +0530

    Updated comments model

--- src/models/comments.ts
+++ src/models/comments.ts
@@ -5,6 +5,8 @@ export interface IComment extends Document{
     text:string;
     date:Date;
     movie_id:Schema.Types.ObjectId;
+    isParent:boolean;
+    parent_id:Schema.Types.ObjectId;
 }
 
 const commentSchema = new Schema<IComment>({
@@ -12,7 +14,9 @@ const commentSchema = new Schema<IComment>({
     email:{type:String, required:true},
     text:{type:String, required:true},
     date:{type:Date, default:Date.now},
-    movie_id:{type:Schema.Types.ObjectId, ref:'Movie'}
+    movie_id:{type:Schema.Types.ObjectId, ref:'Movie'},
+    isParent:{type:Boolean, default:true}, 
+    parent_id:{type:Schema.Types.ObjectId, ref:'Comment'},
 },{timestamps:true});
 
 const Comment = model<IComment>('Comment', commentSchema);
