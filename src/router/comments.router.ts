e5c9623ad9fbffaea45d1ca38631abac917801f6
Yuvraj Singh <yuvraj.singh@xarterian.com>
2025-01-20 18:34:38 +0530

    Added post reply route

--- src/router/comments.router.ts
+++ src/router/comments.router.ts
@@ -1,10 +1,13 @@
 import express from 'express'; 
-import {fetchComments} from '../controllers/comments.controller';
+import {fetchComments, postComment, postReply} from '../controllers/comments.controller';
+import { verifyToken } from '../middleware/authMiddleware';
+
 
 
 const router = express.Router();
 
 router.get('/:movie_id', fetchComments);
-// router.post('/', postcomment);
+router.post('/',verifyToken, postComment);
+router.post('/reply',verifyToken, postReply);
 
 export default router;
